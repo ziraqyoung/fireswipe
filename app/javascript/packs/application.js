c43af207650643bdaee8b61afea70bc9a291f389
ziraqyoung <me_ziraq@yahoo.com>
2019-11-30 02:03:50 +0300

    - Add css to post homepage - Create home.js for random colors to homepage - Modify _post.html.erb for bottom margin and rounded bootstrap class

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,7 @@ require("channels");
 
 import "bootstrap";
 import "../stylesheets/application.scss";
-
-document.addEventListener("turbolinks:load", () => {
-  // Code goes here....
-});
+import "packs/pages/home";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
