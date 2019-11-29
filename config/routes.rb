d6b59e803dc33baae9cf1d1423cc934a887e91cb
ziraqyoung <me_ziraq@yahoo.com>
2019-11-29 23:24:55 +0300

    - Generate Posts controller - create index action to retrieve posts - Declare routes for posts - create _post.html.erb and render posts in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'pages#index'
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
