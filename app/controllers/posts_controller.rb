6407e217ac911ee87e8b37e3081e5fc912cb32b3
ziraqyoung <me_ziraq@yahoo.com>
2019-12-03 21:41:51 +0300

    = Create branch pages for specific posts - Inside PostController, define hobby, team and study actions - Define post_for_branch method to call inside this actions - Create _branch.html.haml partial -

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def hobby
+    posts_for_branch(params[:action])
+  end
+
+  def study
+    posts_for_branch(params[:action])
+  end
+
+  def team
+    posts_for_branch(params[:action])
+  end
+
+  private
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = fetch_posts.paginate(page: params[:page])
+  end
+
+  def fetch_posts
+    Post.limit(30)
+  end
 end
