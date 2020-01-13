234905e9a4ae7fcbc83d40a22171580e2e0f63b6
ziraqyoung <me_ziraq@yahoo.com>
2020-01-13 10:51:02 +0300

    Add request spec for new post template

---
+++ spec/requests/posts/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'new', type: :request do
+  context 'non-signed in user' do
+    it 'redirects to root path' do
+      get '/posts/new'
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  context 'signed-in user' do
+    let(:user) { create(:user) }
+    before(:each) { login_as user }
+    it 'renders a new template' do
+      get '/posts/new'
+      expect(response).to render_template(:new)
+    end
+  end
+end
