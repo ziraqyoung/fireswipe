2552064a558acdb58db7d95dba764927fe3d1ed3
ziraqyoung <ziraqyoung@outlook.com>
2020-03-06 16:34:53 +0300

    Define private_conversations_windows in ApplicationHelper - fixed typo in _private_conversations_windows.html.haml - render _private_conversations_windows.html.haml in application.html.haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   include Private::ConversationsHelper
   include NavigationHelper
   include PostsHelper
+
+  def private_conversations_windows
+    params[:controller] != 'messengers' ? @private_conversations_windows : []
+  end
 end
